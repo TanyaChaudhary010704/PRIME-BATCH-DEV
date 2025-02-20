7927f4d6a0ecf3d352289d9fef1a23ce2e8c9185
TanyaChaudhary010704 <punch010704@gmail.com>
2025-02-20 16:52:05 +0530

    Array methods

---
+++ day5-JS/array.js
@@ -0,0 +1,24 @@
+//---------------------------------------------------------
+//INDEXOF()
+
+//PRIMITIVE ELEMENTS
+
+// let arr = [1,2,3,4,5,6,7,8];
+// let position = arr.indexOf(8);
+// console.log(position);
+
+let arr = [1,8,3,8,5,6,7,8];
+let position = arr.indexOf(8);
+console.log(position);
+
+//NON PRIMITIVE ELEMENTS - have different address
+
+// let arr = [{ id: 1 }, { id: 7 }];
+// let position = arr.indexOf({ id: 7 });
+// console.log(position);
+
+// let arr = [[1,2],[3,4],[5,6],[6,7]];
+// let position = arr.indexOf([5,6]);
+// console.log(position);
+
+//---------------------------------------------------------
